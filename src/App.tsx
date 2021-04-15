dfac5360d5faddc8d9e3a3692864bd8403f73307
Ezequiel Fonseca <ezequiel_fs13@hotmail.com>
2021-04-14 22:45:56 -0300

    add Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 import AboutUs from "../src/pages/AboutUs"
+import GlobalStyle from "./components/GlobalStyle";
 
 const App:FC = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs/>
     </div>
   );
--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,34 +1,66 @@
-import { FC } from 'react';
-
+import { FC } from "react";
+import styled from "styled-components";
 import bruxo from "../assets/images/bruxo.jpg"
 
 const AboutSection: FC = () => {
     return (
-        <div className="AboutSection">
-            <div className="description">
+        <About>
+            <Description>
                 <div>
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>
                             your <span> dreams </span> come
                         </h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                 </div>
-            </div>
-            <p>
-                Bring your "dibre" and I will give you the right spell.
-            </p>
-            <button> Contact Us </button>
-            <div className="image">
-                <img src={bruxo} alt="bruxo"/>
-            </div>
-        </div>
+                <p>
+                    Bring your "dibre" and I will give you the right spell.
+                </p>
+                <button> Contact Us </button>
+            </Description>
+            <Image>
+                <img src={bruxo} alt="bruxo" />
+            </Image>
+        </About>
     );
 }
+
+const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content:space;
+    padding: 5rem 10rem;
+    color: white;
+`;
+
+const Description = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2{
+        font-weight:lighter;
+    }
+`
+
+const Image = styled.div `
+    flex: 1;
+    overflow: hidden;
+    img {
+        width:100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`
+
+const Hide = styled.div`
+    overflow: hidden;
+`
+
 
 export default AboutSection;
